f4ef016d0302f1624557e55cef7afa21e920224e
oyunkatalogu <mehmettasdemircv@gmail.com>
2025-05-04 00:32:57 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,1 @@
-<!-- firebase.js -->
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyBb1dqb3SqYpkEtB3Wnt8LSVc41TSGETe4",
-    authDomain: "akmegamestore-66522.firebaseapp.com",
-    databaseURL: "https://akmegamestore-66522-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "akmegamestore-66522",
-    storageBucket: "akmegamestore-66522.appspot.com",
-    messagingSenderId: "893039272243",
-    appId: "1:893039272243:web:c1b862e113b1e1a9528023"
-  };
 
-  firebase.initializeApp(firebaseConfig);
-</script>
